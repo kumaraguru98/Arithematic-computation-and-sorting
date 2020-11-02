4319f4350f3b9501477ba7d42f56f57fa32bfa13
baluguru <smkumarag@gmail.com>
2020-11-03 00:00:59 +0530

    added first_computation

--- arithematicandsorting.sh
+++ arithematicandsorting.sh
@@ -5,3 +5,6 @@ echo "welcome to arithematic and sorting problem"
 read -p "enter value: " a
 read -p "enter value: " b
 read -p "enter value: " c
+
+first compute=$(( $a+$b+$c ))
+echo first compute:"$first compute"
